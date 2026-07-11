7c933d24935020bb31e49bd7ca741962b8dd9d49
Smolniiik <grisha.smolnikov@gmail.com>
2026-07-11 12:08:17 +0300

    Добавил Employee в админку

--- game_post_1/other_pages/models.py
+++ game_post_1/other_pages/models.py
@@ -2,14 +2,45 @@ from django.db import models
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=128, blank=False, null=False)
-    middle_name = models.CharField(max_length=128, blank=True, null=True)
-    surname = models.CharField(max_length=128, blank=False, null=False)
-    position_eng = models.CharField(max_length=128)
-    position_ru = models.CharField(max_length=128)
-    position_description = models.CharField(max_length=256)
-    link_vk = models.URLField(max_length=500)
-    link_tg = models.URLField(max_length=500)
-    link_mail = models.URLField(max_length=500)
-    photo = models.ImageField(upload_to='avatars/')
-    active = models.BooleanField(default=True)
+    name = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False,
+    )
+    middle_name = models.CharField(
+        max_length=128,
+        blank=True,
+        null=True,
+    )
+    surname = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False,
+    )
+    position_eng = models.CharField(
+        max_length=128,
+    )
+    position_ru = models.CharField(
+        max_length=128,
+    )
+    position_description = models.CharField(
+        max_length=256,
+    )
+    link_vk = models.URLField(
+        max_length=500,
+    )
+    link_tg = models.URLField(
+        max_length=500,
+    )
+    link_mail = models.URLField(
+        max_length=500,
+    )
+    photo = models.ImageField(
+        upload_to='avatars/',
+    )
+    active = models.BooleanField(
+        default=True,
+    )
+
+    class Meta:
+        verbose_name = 'Сотрудники'
